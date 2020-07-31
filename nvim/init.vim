a71814a0d8a5e9dff1ee464522a37a07c5bf21f8
YunRongLi <martianlee@gms.tku.edu.tw>
2020-07-31 15:37:49 +0800

    Init Git

---
+++ nvim/init.vim
@@ -0,0 +1,12 @@
+"----------------"
+" Plugins
+"----------------"
+call plug#begin('~/.config/nvim/plugged')
+    
+    Plug 'nvim-treesitter/nvim-treesitter' 
+    Plug 'sheerun/vim-polyglot'
+    " Color Schemes"
+    Plug 'sainnhe/gruvbox-material'
+
+call plug#end()
+
---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+PWD=$(pwd)
+
+PYTHON3=$(which python3)
+NVIMBIN="$PWD/bin/nvim"
+NVIMPATH="PATH=$PWD/bin:\$PATH"
+#function installYouCompleteMe() {
+#    $PYTHON3 ./plugged/YouCompleteMe/install.py \
+#                              --clang-completer \
+#                                 --go-completer \
+#                                --ts-completer
+#}
+
+function downloadNeoVim() {
+    wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+}
+
+if [ "$1" = "install" ];then
+    if [ ! -f $PWD/nvim.appimage ];then
+        $(downloadNeoVim)
+    fi
+    chmod +x $PWD/nvim.appimage
+
+    ln -s $PWD/nvim.appimage $NVIMBIN
+    $(echo "export $NVIMPATH" >> ~/.bashrc)
+    source ~/.bashrc
+
+	if ! type nvim > /dev/null; then
+        echo "Neovim not found"
+        exit
+    fi
+	ln -s $PWD/nvim ~/.config/nvim
+    $NVIMBIN -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
+    #$(installYouCompleteMe)
+    echo "Setup Done"
+fi
+
+if [ "$1" = "uninstall" ];then
+    rm ~/.config/nvim
+    echo "Delete .vim Symbolic link"
+fi
+
+
+
