cfb6e3f733d78624d80e707f857a7df07c18d0a8
YunRongLi <martianlee@gms.tku.edu.tw>
2020-08-27 09:41:10 +0800

    Update setup.sh for suport mac

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 PWD=$(pwd)
 
 PYTHON3=$(which python3)
-NVIMBIN="$PWD/bin/nvim"
 NVIMPATH="PATH=$PWD/bin:\$PATH"
 #function installYouCompleteMe() {
 #    $PYTHON3 ./plugged/YouCompleteMe/install.py \
@@ -13,24 +12,41 @@ NVIMPATH="PATH=$PWD/bin:\$PATH"
 #}
 
 function downloadNeoVim() {
-    wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+    wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 }
 
 if [ "$1" = "install" ];then
-    if [ ! -f $PWD/nvim.appimage ];then
-        $(downloadNeoVim)
+    if [[ "$OSTYPE" == "linux-gnu"* ]];then
+        if [ ! -f $PWD/nvim.appimage ];then
+            $(downloadNeoVim) && chmox +x $PWD/nvim.appimage && \
+                mkdir $PWD/bin && ln -s $PWD/nvim.appimage $PWD/bin/nvim && \
+                $(echo "export $NVIMPATH" >> ~/.bashrc)
+            source ~/.bashrc
+        fi
+    elif [[ "$OSTYPE" == "darwin"* ]];then
+        if [ ! $(which nvim) ];then
+            echo "Not found NeoVim."
+            if [ $(which brew) ];then
+                "Install NeoVim by brew."
+                brew install nvim
+            else
+                echo "Please install brew, and try again."
+                exit
+            fi
+        fi
     fi
-    chmod +x $PWD/nvim.appimage
-
-    ln -s $PWD/nvim.appimage $NVIMBIN
-    $(echo "export $NVIMPATH" >> ~/.bashrc)
-    source ~/.bashrc
 
 	if ! type nvim > /dev/null; then
         echo "Neovim not found"
         exit
     fi
+	
+	if [ ! -d ~/.config ];then
+	    mkdir ~/.config
+	fi
 	ln -s $PWD/nvim ~/.config/nvim
+   
+    NVIMBIN=$(which nvim) 
     $NVIMBIN -es -u $PWD/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
     #$(installYouCompleteMe)
     echo "Setup Done"
